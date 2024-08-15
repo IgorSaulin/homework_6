0973825d0eb2d116994675cbfdd4c61e42b2bc30
Igor Saulin <ig.saulin@yandex.ru>
2024-08-15 23:52:40 +0300

    Reload operators

--- src/Matrix.cpp
+++ src/Matrix.cpp
@@ -24,6 +24,28 @@ real& Matrix::operator()(int row, int col)
     return this->mvec_.at(pos);
 }
 
+real Matrix::operator()(int row, int col) const
+{
+    if (row >= this-> rows_)
+    {
+        std::cerr << "Matrix: row number out of bounds" << std::endl;
+        //return 0;
+    }
+
+    if (col >= this-> cols_)
+    {
+        std::cerr << "Matrix: col number out of bounds" << std::endl;
+        //return 0;
+    }
+
+    int pos(0);
+
+    pos = cols_*row + col;
+
+    return this->mvec_.at(pos);
+    
+}
+
 void Matrix::print()
 {
     for(int i=0; i<this->rows_; ++i)
@@ -35,4 +57,65 @@ void Matrix::print()
 
         std::cout<<std::endl;
     }
+}
+
+Matrix math::operator+(const Matrix &A, const Matrix &B)
+{
+    if((A.cols_ != B.cols_) || (A.rows_ != B.rows_))
+    {
+        std::cerr<<"Matrix: Matrices can't be added"<<std::endl;
+        return Matrix(0, 0);
+    }
+
+    Matrix M(A.cols_, A.rows_);
+
+    for(int i=0; i<M.mvec_.size(); ++i)
+    {
+        M.mvec_.at(i) = A.mvec_.at(i)+B.mvec_.at(i);
+    }
+
+    return M;
+}
+
+Matrix math::operator-(const Matrix &A, const Matrix &B)
+{
+    if((A.cols_ != B.cols_) || (A.rows_ != B.rows_))
+    {
+        std::cerr<<"Matrix: Matrices can't be subtracted!"<<std::endl;
+        return Matrix(0, 0);
+    }
+
+    Matrix M(A.cols_, A.rows_);
+
+    for(int i=0; i<M.mvec_.size(); ++i)
+    {
+        M.mvec_.at(i) = A.mvec_.at(i)-B.mvec_.at(i);
+    }
+
+    return M;
+}
+
+Matrix math::operator*(const Matrix &A, const Matrix &B)
+{
+    if (A.cols_!=B.rows_)
+    {
+        std::cerr<<"Matrix: Matrices can't be multiplied!"<<std::endl;
+        return Matrix(0, 0);
+    }
+
+    Matrix M(A.rows_, B.cols_);
+
+    //Следующий алгоритм удобен для распаралеливания многопоточных систем
+    for(int pos=0; pos<M.mvec_.size(); ++pos)
+    {
+        int row = (int)std::floor(pos/M.cols_);
+        int col = pos - row*M.cols_;
+
+        for (int k=0; k<A.cols_; ++k)
+        {
+            M.mvec_.at(pos) += A(row,k)*B(k,col);
+        }
+    }
+
+    return M;
 }
--- src/Matrix.h
+++ src/Matrix.h
@@ -12,14 +12,25 @@ namespace math //Задается пространство имен, в кото
         private:
             int cols_; //Переменные отвечающие за колонку и за строку
             int rows_;
-            std::vector<real> mvec_;
+            std::vector<real> mvec_; //Хранит элементы матрицы в виде одномерного массива
         public:
-            Matrix() {};
+            Matrix() {}; //Конструктор по умолчанию
+
             //Конструтор с параметрами по умолчанию
             Matrix(int rows, int cols) : cols_(cols), rows_(rows), mvec_(std::vector<real>(rows*cols)) {};
 
-            real& operator()(int row, int col);
+            real& operator()(int row, int col); //Доступ к элементам матрицы
 
-            void print();
+            real operator()(int row, int col) const; //Ключевое слово const указанное после функции гарантирует, что функция не изменяет полей данного класса
+
+            void print(); //Функция вывода информации в консоль
+
+            /*Перегрузка оператора сложения для матрицы, реализована через дружественные функции*/
+
+            friend Matrix operator+(const Matrix& A, const Matrix& B);
+
+            friend Matrix operator-(const Matrix& A, const Matrix& B);
+
+            friend Matrix operator*(const Matrix& A, const Matrix& B);
     };
 }
